4ec538cce3e124e91165a2a19dc7bca584de6d50
micha kops <webmaster@hascode.com>
2011-06-20 20:25:24 +0200

    Example for Portlet modes mapping in the JSF bridge added

--- src/main/java/com/hascode/tutorial/jsf/UserBean.java
+++ src/main/java/com/hascode/tutorial/jsf/UserBean.java
@@ -1,12 +1,53 @@
 package com.hascode.tutorial.jsf;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.portlet.ActionResponse;
+import javax.portlet.PortletMode;
+import javax.portlet.PortletModeException;
 
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class UserBean {
-	public String[] getUserNames() {
-		return new String[]{"adam", "barry", "douglas", "ethan"};
+	private String				sortType	= "normal";
+
+	private final List<String>	names		= new ArrayList<String>();
+	{
+		names.add("adam");
+		names.add("barry");
+		names.add("douglas");
+		names.add("ethan");
+	}
+
+	public List<String> getUserNames() {
+		if ("reverse".equals(sortType)) {
+			Collections.reverse(names);
+			return names;
+		}
+
+		Collections.sort(names);
+		return names;
+	}
+
+	public void save() throws PortletModeException {
+		// Switch the portlet mode back to VIEW.
+		FacesContext facesContext = FacesContext.getCurrentInstance();
+		ExternalContext externalContext = facesContext.getExternalContext();
+		ActionResponse actionResponse = (ActionResponse) externalContext.getResponse();
+		actionResponse.setPortletMode(PortletMode.VIEW);
+	}
+
+	public String getSortType() {
+		return sortType;
+	}
+
+	public void setSortType(String sortType) {
+		this.sortType = sortType;
 	}
 }
